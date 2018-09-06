04ee699d93ff52ffbd85d181a106d578606216a2
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-09-06 14:52:24 -0400

    input

--- rover.py
+++ rover.py
@@ -3,9 +3,11 @@
 #rover.py
 
 def main():
-	distance = input("How far is the light travelling? Just provide the number in miles. ")
+	distance = eval(input("How far is the light travelling? Just provide the number in miles. "))
 speedOfLight = 186000 #in miles
 time = distance / speedOfLight
 #time = 34000000/186000
 print("It would take ", time, " seconds for the Mars Curiosity rocer to send photos from Mars to Earth") 
+
+main()
 
