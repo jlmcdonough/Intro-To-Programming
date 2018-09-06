85a8bbeb2e1d9be7a48ef53e052f648df56f7729
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-09-06 14:19:50 -0400

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,10 @@
+#Joseph McDonough
+#September 6 2018
+#rover.py
+
+def main():
+	distance = input("How far is the light travelling? ")
+	time = distance * 186000
+	print("It would take " + time + " seconds")
+
+main()
