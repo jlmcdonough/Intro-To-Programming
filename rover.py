e936f87eaf64cba1e10685d70f254a5f3d2af45f
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-09-06 15:04:09 -0400

    Curiosity Rover Exercise

--- rover.py
+++ rover.py
@@ -2,11 +2,11 @@
 #September 6 2018
 #rover.py
 
-def main():
+#def main():
 	#distance = float(input("How far is the light travelling? Just provide the number in miles. "))
 #speedOfLight = 186000.0 #in miles
 #time = distance / 186000
-float time = 34000000/186000  
+time = 34000000/186000  
 print("It would take ", time, " seconds for the Mars Curiosity rocer to send photos from Mars to Earth") 
 
-main()
+#main()
