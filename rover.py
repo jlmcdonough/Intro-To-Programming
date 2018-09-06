f1794e0b82f1f69e876a89333e9cfd18f7cd9162
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-09-06 14:58:32 -0400

    plox

--- rover.py
+++ rover.py
@@ -3,11 +3,10 @@
 #rover.py
 
 def main():
-	distance = float(input("How far is the light travelling? Just provide the number in miles. ")
-speedOfLight = 186000 #in miles
-time = distance / speedOfLight
+	distance = float(input("How far is the light travelling? Just provide the number in miles. "))
+#speedOfLight = 186000.0 #in miles
+time = distance / 186000
 #time = 34000000/186000
 print("It would take ", time, " seconds for the Mars Curiosity rocer to send photos from Mars to Earth") 
 
 main()
-
