5a22fc01b75f90f6e3bc1deeb06bf5822ae0dc5a
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-09-06 14:40:01 -0400

    ...

--- rover.py
+++ rover.py
@@ -5,6 +5,6 @@
 #def main():
 	#distance = input("How far is the light travelling? Just provide the number in miles. ")
 	#time = distance / 186000
-	time = 34000000 / 186000
-	print("It would take " + time + " seconds")
+time = 34000000 / 186000
+print("It would take " + time + " seconds")
 
