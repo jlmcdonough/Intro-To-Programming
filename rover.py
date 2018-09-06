e77562fef796749af7eae23bac9967afefa63450
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-09-06 14:23:59 -0400

    updated header

--- rover.py
+++ rover.py
@@ -1,9 +1,9 @@
-#Joseph McDonough
+#Joseph McDonough CMPT 120L-113
 #September 6 2018
 #rover.py
 
 def main():
-	distance = input("How far is the light travelling? ")
+	distance = input("How far is the light travelling? Just provide the number. ")
 	time = distance * 186000
 	print("It would take " + time + " seconds")
 
