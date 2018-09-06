794dea9decacede206a2d39ff582bf5b9371eaae
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-09-06 14:38:41 -0400

    ,,,

--- rover.py
+++ rover.py
@@ -2,10 +2,9 @@
 #September 6 2018
 #rover.py
 
-def main():
+#def main():
 	#distance = input("How far is the light travelling? Just provide the number in miles. ")
 	#time = distance / 186000
 	time = 34000000 / 186000
 	print("It would take " + time + " seconds")
 
-main()
