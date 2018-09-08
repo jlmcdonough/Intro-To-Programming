581cd02a02830400ac2f822ec5946730b30183ee
jmcdonough006 <42847035+jmcdonough006@users.noreply.github.com>
2018-09-08 14:19:35 -0400

    Solution to Mad Libs exercise

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+#Joseph McDonough CMPT 120-113
+#Lab 2 - Mad Libs
+#9/6/18
+def main():
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adjective = input("Enter an adjective: ")
+    place = input("Enter a plcae: ")
+    print("Take your " + adjective + " " + noun + " and " + verb + " it at the " + place + "!")
+
+main()
+    
