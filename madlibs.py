d7ce706000904a92cb44d7f754693af484d0bb46
jmcdonough006 <Joseph.McDonough1@marist.edu>
2018-09-13 14:11:37 -0400

    Updated madlibs

--- madlibs.py
+++ madlibs.py
@@ -1,12 +1,20 @@
 #Joseph McDonough CMPT 120-113
 #Lab 2 - Mad Libs
 #9/6/18
-def main():
+def promptForWords():
+    'noun,verb,adjective,place'
     noun = input("Enter a noun: ")
     verb = input("Enter a verb: ")
     adjective = input("Enter an adjective: ")
     place = input("Enter a plcae: ")
-    print("Take your " + adjective + " " + noun + " and " + verb + " it at the " + place + "!")
+   
+def makeAndPrintSentence():
+ print("Take your " + adjective + " " + noun + " and " + verb + " it at the " + place + "!")
+
+def main():
+	promptForWords()
+	makeAndPrintSentence()
+
 
 main()
     
