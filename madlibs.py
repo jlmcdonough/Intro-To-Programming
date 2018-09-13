22409d4a240c6128dd073bdedc42c0f5af7a6cb8
jmcdonough006 <Joseph.McDonough1@marist.edu>
2018-09-13 14:27:13 -0400

    Made noun, verb, adjective, and place global hopefully

--- madlibs.py
+++ madlibs.py
@@ -1,15 +1,12 @@
 #Joseph McDonough CMPT 120-113
 #Lab 2 - Mad Libs
 #9/6/18
-noun=str("")
-verb=str("")
-adjective=str("")
-place=str("")
+
 def promptForWords():
-    noun = input("Enter a noun: ")
-    verb = input("Enter a verb: ")
-    adjective = input("Enter an adjective: ")
-    place = input("Enter a place: ")
+	global noun = input("Enter a noun: ")
+	global verb = input("Enter a verb: ")
+	global adjective = input("Enter an adjective: ")
+	global place = input("Enter a place: ")
    
 def makeAndPrintSentence():
  print("Take your " + adjective + " " + noun + " and " + verb + " it at the " + place + "!")
