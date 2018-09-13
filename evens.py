0bf610a70a84694271109295e5738fa6e632f92d
jmcdonough006 <Joseph.McDonough1@marist.edu>
2018-09-13 14:08:12 -0400

    Evens updated

--- evens.py
+++ evens.py
@@ -2,15 +2,16 @@
 #September 6 2018
 #evens.py
 
-print("Stated in directions")
-for i in range(10):
-	print(i)
+def main():
+	print("Stated in directions")
+	for i in range(10):
+		print(i)
 
-print(" ")
-print("Print even numbers betwenn 2 and 20")
+	print("\nPrint even numbers betwenn 2 and 20")
+	for j in range(2, 21):
+		if(j%2==0):
+			print(j)
 
-for j in range(2, 21):
-	if(j%2==0):
-		print(j)
+main()
 
 
