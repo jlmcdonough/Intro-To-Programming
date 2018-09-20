d72dbd28fc60ec03527b9b74c577ab7627a2ac5a
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-09-20 14:23:37 -0400

    First test of fibonacci code

---
+++ fibonacci.py
@@ -0,0 +1,28 @@
+#Joseph McDonough
+#CMPT 120L-113
+#20 September 2018
+#Lab 4 - Fibonacci Sequence
+
+def fibonacciSequence(n):
+	global currentValue = 1
+	global previousValue = 1
+	if(n>2):	
+		for x in range(n-2):
+			global nextValue = currentValue+previousValue
+			currentValue=previousValue
+			x++
+	else:
+		return 1
+	return nextValue
+
+
+inputValue = input("Enter how many times you would like the Fibonacci sequence to run: ")
+print("The fibonacci sequence after being ran " + inputValue + " times, the value is " + nextValue)  
+
+			
+			
+			
+		
+	
+	 	
+
