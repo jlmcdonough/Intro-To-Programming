776293589794981250b68a2a9f5629569fcfcc83
jmcdonough006 <42847035+jmcdonough006@users.noreply.github.com>
2018-09-20 15:13:45 -0400

    First attempt at pi.py

---
+++ pi.py
@@ -0,0 +1,16 @@
+#Joseph McDonough
+#CMPT 120L-113
+#Lab 4 pi.py
+
+from math import *
+
+for x in range(n)
+	denominator = 1	
+	if(x%2!=0):
+		answer-= 4/denominator
+	else:
+		answer+=4/denominator
+	denominator+=2
+
+print(answer)
+print(math.pi)
