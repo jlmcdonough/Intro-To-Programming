b774cd56690e375f7489f058ee8f99e3dbf6d99b
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-09-20 14:33:44 -0400

    9

--- fibonacci.py
+++ fibonacci.py
@@ -6,18 +6,20 @@
 inputValue = input("Enter how many times you would like the Fibonacci sequence to run: ")
 print("The fibonacci sequence after being ran " + inputValue + " times, the value is " + fibonacciSequence(inputValue))  
 
-currentValue = 1
-previousValue = 1
-global nextValue
-#nextValue = 2
-if(n>2):	
-	for x in range(n-2):
-		nextValue = currentValue+previousValue
-		currentValue=previousValue
-		#x++
-else:
-	return 1
-return nextValue
+def fibonacciSequence(n)
+	currentValue = 1
+	previousValue = 1
+	global nextValue
+	#nextValue = 2
+	if(n>2):	
+		for x in range(n-2):
+			nextValue = currentValue+previousValue
+			currentValue=previousValue
+			answer=nextValue
+			#x++
+	else:
+		return answer
+	return answer
 
 			
 			
