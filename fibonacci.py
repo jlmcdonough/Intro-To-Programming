e95f04efe38fae57e9b430e51709df67c90a7c15
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-09-20 14:59:58 -0400

    21

--- fibonacci.py
+++ fibonacci.py
@@ -6,7 +6,7 @@
 def fibonacciSequence(n):
     currentValue = 0
     nextValue = 1
-    while nextValue <= n:
+    for x in range(n):
         previousValue = currentValue
         currentValue = nextValue
         nextValue += previousValue
