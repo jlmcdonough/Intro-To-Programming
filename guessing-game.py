923bf12fed5958f8204d1bf33593d321519151a6
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-10-18 14:16:33 -0400

    Initial version of a simple guessing game.

--- guessing-game.py
+++ guessing-game.py
@@ -1,18 +1,16 @@
 def main():
-	print("PYTHON GUESSING GAME.")
-	animalName = "lion"
-	guess=""	
-	guessNorm=""
-	while True:  #instructions state test is always true
-		print("I am thinking of an animal.")		
-		guess=str(input("Enter your guess: ")
-		guessNorm=guess.strip().lower()
-		if(guessNorm==animalName):
-			print("You guessed correct! Congratulations!")
-			break
-		else:
-			print("You guess was wrong please try again.")
+    print("PYTHON GUESSING GAME.")
+    animalName = "lion"
+    userInput=""    
+    guess=""
+    while True:  #instructions state test is always true
+        print("I am thinking of an animal.")        
+        userInput = str(input("Enter your guess: "))
+        guess = userInput.strip().lower()
+        if(guess==animalName):
+            print("You guessed correct! Congratulations!")
+            break
+        else:
+            print("You guess was wrong please try again.")
 
 main()
-		
-		
