bb05f575433728255fa91c2c120a75b9f63b9764
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-10-25 14:25:10 -0400

    Arithmetic Enginge Part 1

---
+++ ArithmeticEngine.py
@@ -0,0 +1,43 @@
+# CMPT 120 - Lab #6
+# Joseph McDonough
+# 25 October 2018
+###
+
+def showIntro():
+    print("Welcome to the Arithmetic Engine!")
+    print("=================================\n")
+    print('Valid commands are "add", "mult", "sub", "div", "pow", and "quit".\n')
+
+def showOutro():
+    print("\nThank you for using the Arithmetic Engine…")
+    print("\nPlease come back again soon!")
+
+def doLoop():
+    while True:
+        cmd = input("What computation do you want to perform? ")
+        cmd=cmd.strip().lower()
+        if(cmd=="add" or cmd=="sub" or cmd=="mult" or cmd=="div" or cmd=="pow"):    
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input("Enter the second number: "))
+            if (cmd == "add"):
+                result = num1 + num2
+            elif (cmd == "sub"):
+                result = num1 - num2
+            elif (cmd == "mult"):
+                result = num1 * num2
+            elif (cmd == "div"):
+                result = num1 // num2
+            elif (cmd == "pow"):
+                result == num1**num2
+        elif (cmd == "quit"):
+            break
+        else:
+            print(cmd + " is not a valid command.")
+        print("The result is " + str(result) + ".\n")
+
+def main():
+    showIntro()
+    doLoop()
+    showOutro()
+
+main()
