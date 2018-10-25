a47fd08450a207d8307724a38bbc44e17ebf7995
Joseph McDonough <joseph.mcdonough1@marist.edu>
2018-10-25 14:42:02 -0400

    Part 2 of the Arithmetic Enging Lab

--- ArithmeticEngine.py
+++ ArithmeticEngine.py
@@ -3,8 +3,6 @@
 # 25 October 2018
 ###
 
-print(2**3)
-
 def showIntro():
     print("Welcome to the Arithmetic Engine!")
     print("=================================\n")
@@ -19,25 +17,29 @@ def doLoop():
         cmd = input("What computation do you want to perform? ")
         cmd=cmd.strip().lower()
         if(cmd=="add" or cmd=="sub" or cmd=="mult" or cmd=="div" or cmd=="pow"):    
-            num1 = int(input("Enter the first number: "))
-            num2 = int(input("Enter the second number: "))
-            if (cmd == "add"):
-                result = num1 + num2
-            elif (cmd == "sub"):
-                result = num1 - num2
-            elif (cmd == "mult"):
-                result = num1 * num2
-            elif (cmd == "div"):
-                result = num1 // num2
-            elif (cmd == "pow"):
-                result = num1**num2
+            try:
+                num1 = int(input("Enter the first number: "))
+                num2 = int(input("Enter the second number: "))
+                if (cmd == "add"):
+                    result = num1 + num2
+                elif (cmd == "sub"):
+                    result = num1 - num2
+                elif (cmd == "mult"):
+                    result = num1 * num2
+                elif (cmd == "div"):
+                    result = num1 // num2
+                elif (cmd == "pow"):
+                    result = num1**num2
+                print("The result is " + str(result) + ".\n")
+            except:
+                print("Those aren't numbers. Please try again.")
+                continue
         elif (cmd == "quit"):
             break
         else:
             print(cmd + " is not a valid command.")
             continue
-        print("The result is " + str(result) + ".\n")
-
+        
 def main():
     showIntro()
     doLoop()
